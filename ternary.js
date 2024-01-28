bc3468d578474a5b731837e0c9c600c71042762a
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-01-28 20:42:02 +0600

    solved ternary  problems

--- ternary.js
+++ ternary.js
@@ -13,3 +13,23 @@ also write it  using ternery operator
 
 
 */
+
+const num1 = 10;
+const num2 = 20;
+let result;
+
+if (num1 > num2) {
+  result = num1 * 2;
+  console.log(result);
+} else {
+  result = num1 + num2;
+  console.log(result);
+}
+
+const nums1 = 80;
+const nums2 = 70;
+let results;
+
+nums1 > nums2
+  ? console.log((results = nums1 * 2))
+  : console.log((results = nums1 + nums2));
