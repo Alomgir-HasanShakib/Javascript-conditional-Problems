d50d8346dfb8221c628eb5e1f05e5313b3097abc
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-01-28 20:01:33 +0600

    solve ticket fare problem

--- bus.js
+++ bus.js
@@ -8,3 +8,16 @@ Ticket fare Calculator :
     --otherwise Regular Ticket fare 800tk
 
 */
+
+const students = false;
+const age = 70;
+
+if (students == true) {
+  console.log("You'r get 50% Discount");
+} else if (age >= 60) {
+  console.log("You got 15% discounts");
+} else if (age <= 10) {
+  console.log("You don't need any ticket You'free in our bus");
+} else {
+  console.log("You have to pay 800 tk");
+}
